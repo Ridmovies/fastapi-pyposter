ee8a8ba7c41c1fbf09d3140b336c55e489986de2
riddler_rid <unkownrid@gmail.com>
2025-02-21 12:09:42 +0300

    add post crud

--- src/main.py
+++ src/main.py
@@ -4,5 +4,6 @@ from src.dev.router import dev_router
 from src.posts.router import post_router
 
 app = FastAPI()
-app.include_router(dev_router, prefix="/dev", tags=["dev"])
 app.include_router(post_router, prefix="/posts", tags=["posts"])
+app.include_router(dev_router, prefix="/dev", tags=["dev"])
+
--- src/posts/router.py
+++ src/posts/router.py
@@ -1,5 +1,6 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from sqlalchemy import select
+from starlette import status
 
 from src.database import SessionDep
 from src.posts.models import Post
@@ -20,5 +21,35 @@ async def get_all_posts(session: SessionDep):
 async def create_post(session: SessionDep, post_data: PostCreate):
     post = Post(**post_data.model_dump())
     session.add(post)
+    await session.commit()
+    return post
+
+
+@post_router.delete("/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post(session: SessionDep, post_id: int):
+    stmt = select(Post).where(Post.id == post_id)
+    result = await session.execute(stmt)
+    post = result.scalar_one_or_none()
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Post not found'
+        )
+    await session.delete(post)
+    await session.commit()
+    return {'message': 'Post deleted successfully'}
+
+
+@post_router.put("/{post_id}")
+async def update_post(session: SessionDep, post_id: int, post_data: PostCreate):
+    stmt = select(Post).where(Post.id == post_id)
+    result = await session.execute(stmt)
+    post = result.scalar_one_or_none()
+    if not post:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Post not found'
+        )
+    post.content = post_data.content
     await session.commit()
     return post
